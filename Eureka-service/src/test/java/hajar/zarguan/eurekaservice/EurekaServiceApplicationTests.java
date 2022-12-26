391d6918aedff41cd82e96049878df2ff167dff9
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-12-26 10:59:19 +0100

    Eureka server

---
+++ Eureka-service/src/test/java/hajar/zarguan/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hajar.zarguan.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
