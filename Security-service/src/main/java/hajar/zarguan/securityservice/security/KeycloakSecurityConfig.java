8b1a5298d787182b1100fe46369bf57e9a0f4cc6
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-12-26 11:02:53 +0100

    Security Keyloack Config

---
+++ Security-service/src/main/java/hajar/zarguan/securityservice/security/KeycloakSecurityConfig.java
@@ -0,0 +1,32 @@
+package hajar.zarguan.securityservice.security;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    //    http.authorizeRequests().antMatchers("/microservice1/**","/microservice2/**" ).authenticated();
+        http.csrf().disable().cors().and().headers().frameOptions().disable()
+                .and()
+                .authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated();
+    }
+}
---
+++ Security-service/src/test/java/hajar/zarguan/securityservice/SecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hajar.zarguan.securityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
