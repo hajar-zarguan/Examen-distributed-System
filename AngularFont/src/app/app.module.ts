afbffe29af5e03d5bbd1bbf02ccba3ff2f37d094
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-12-26 12:01:41 +0100

    Frontend Angular Init

---
+++ AngularFont/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ListeComponent } from './liste/liste.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ListeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AngularFont/src/app/liste/liste.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste',
+  templateUrl: './liste.component.html',
+  styleUrls: ['./liste.component.css']
+})
+export class ListeComponent {
+
+}
